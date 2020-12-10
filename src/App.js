06cd956ffb381f2674ae10f5ac08073571f7da13
S. Aziz <asazizmn@gmail.com>
2020-12-10 16:21:56 +0800

    add ContactList component and empty main App fn

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+class Contactlist extends React.Component {
+  render() {
+
+    // an array of objects
+    const people = [
+      { name: 'Tyler' },
+      { name: 'Karen' },
+      { name: 'Richard' }
+    ]
+
+    // return jsx generated from array of objects above
+    // please note that it is considered good practice to wrap multiline jsx within paranthesis
+    return (
+      <ol>
+        {
+          // please note the implicit return from es6 function using `()` parathesis
+          people.map(person =>
+            <li key={person.name}>{person.name}</li>
+          )
+        }
+      </ol>
+    )
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, World!</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
       </header>
     </div>
   );
